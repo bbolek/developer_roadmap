64b63ac16824f46d7ddcc77a564ace9256479625
burak.bolek@authoritypartners.com-at-930184570490 <1CryPcwCAmkBWc4JNp8Js9HOI2PAlvaFtCbp5bVee14=>
2023-02-17 09:55:09 +0300

    Mark as Complete

--- src/app/custom-node.tsx
+++ src/app/custom-node.tsx
@@ -2,18 +2,16 @@ import {Item} from '../../models/item.model';
 import {Handle, Position} from 'reactflow';
 import classNames from 'classnames';
 import {ItemImportance} from '../../enums/item-importance.enum';
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import NodeDetails from '@/app/node-details';
-import OutsideClickHandler from 'react-outside-click-handler';
-import Draggable from 'react-draggable';
-import {Dialog, Transition} from '@headlessui/react';
+import {Dialog} from '@headlessui/react';
 
 const CustomNode = ({data}: {data: Item, isConnectable: any}) => {
     const [displayData, setDisplayData] = useState<boolean>(false);
-    const wrapper = classNames('px-4 py-2 shadow-md rounded-md border-2 border-stone-400 relative', {
+    const wrapper = classNames('px-4 py-2 shadow-md rounded-md border border-slate-400 relative', {
         'bg-red-500 text-white': data.importance === ItemImportance.MustKnow,
         'bg-orange-500 text-white': data.importance === ItemImportance.BetterToKnow,
-        'bg-yellow-500 text-white': data.importance === ItemImportance.Suggested
+        'bg-sky-500 text-white': data.importance === ItemImportance.Suggested
     }
 )
     const display = () => {
@@ -23,18 +21,19 @@ const CustomNode = ({data}: {data: Item, isConnectable: any}) => {
     return <div className={wrapper}>
         <div className="flex group">
             <div className='flex flex-col items-center'>
-                <div className="text-sm pb-2">{data.label}</div>
-                <button className='bg-white border border-blue-500 text-blue-400 px-4 py-1 rounded-md hidden group-hover:block' onClick={() => display()}>Display</button>
+                <div className="text-sm">{data.label}</div>
+                <div className='hidden group-hover:block pt-2'>
+                    <button className='bg-white border border-blue-500 text-blue-400 px-4 py-1 rounded-md' onClick={() => display()}>Display</button>
+                </div>
             </div>
             {displayData &&  <Dialog
                 open={displayData}
-                onClose={() => setDisplayData(false)}
+                onClose={setDisplayData}
                 className="relative z-50"
                 >
-                <div className="fixed inset-0 bg-black/50 blur-lg" aria-hidden="true" />
+                <div className="fixed inset-0 bg-black/200" aria-hidden="true" />
                 <div className="fixed inset-0 flex items-center justify-center p-4">
-
-                    <NodeDetails shortCode={data.shortCode} />
+                    <NodeDetails onComplete={() => setDisplayData(false)} shortCode={data.shortCode} />
                 </div>
             </Dialog> }
         </div>
--- src/app/node-details.tsx
+++ src/app/node-details.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 export interface NodeDetailsProps {
-    shortCode: string
+    shortCode: string;
+    onComplete: () => void;
 }
 
 const NodeDetails = React.forwardRef<HTMLDivElement, NodeDetailsProps>((props: NodeDetailsProps, forwardedRef) => {
-    return <div ref={forwardedRef} className='border border-gray-400 w-[32rem] pt-4 pb-16 top-96 bg-white justify-left text-slate-900 px-4 overflow-auto rounded-xl drop-shadow-lg'>
-        <h3 className='w-full text-2xl sm:text-2xl font-bold'>Resources you can use</h3>
-         <p className='text-slate-600'>You can find the resources below:</p>
-        <div className='pl-4'>
+    return <div ref={forwardedRef} className='border border-gray-400 w-[32rem] pt-4 pb-8 top-96 bg-white justify-left text-slate-900 px-4 overflow-auto rounded-xl drop-shadow-lg divide-y space-y-4'>
+        <div className='flex flex-row justify-between'>
+            <div>
+                <h3 className='w-full text-2xl sm:text-2xl font-bold'>Resources you can use</h3>
+                <p className='text-slate-600'>You can find the resources below:</p>
+            </div>
+            <div>
+                <button onClick={() => props.onComplete()} className='inline-flex justify-center rounded-md border border-red-600 shadow-sm px-2 py-2 bg-white text-base text-red-500 hover:bg-red-50'>Mark as Complete</button>
+            </div>
+        </div>
+        <div className='pl-4 pt-4'>
             <ol className="list-decimal text-sm underline">
                 <li><a href='https://www.vox.com/2014/6/16/18076282/the-internet' rel='noopener noreferrer' target='_blank'>The Internet Explained</a></li>
                 <li><a href='http://web.stanford.edu/class/msande91si/www-spr04/readings/week1/InternetWhitepaper.htm' rel='noopener noreferrer' target='_blank'>How Does the Internet Work?</a></li>
@@ -16,8 +24,8 @@ const NodeDetails = React.forwardRef<HTMLDivElement, NodeDetailsProps>((props: N
                 <li><a href='https://roadmap.sh/guides/what-is-internet' rel='noopener noreferrer' target='_blank'>Introduction to Internet</a></li>
             </ol>
         </div>
-        <div className='divide-y'>
-
+        <div className='pt-4 text-sm text-slate-500'>
+            <span className='font-bold text-gray-900'>Adnan Softic</span> & 146 others completed this.
         </div>
         <div className='pt-4 text-sm text-slate-500'>
             You can get more information from here...
